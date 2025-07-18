c1510337fe08b783808099bfd93b4a08fd3969f3
Talyx <Tylercobey@hotmail.com>
2025-07-18 00:13:03 -0400

    Update scriptfang_gui.py

--- SCRIPTFANG/gui/scriptfang_gui.py
+++ SCRIPTFANG/gui/scriptfang_gui.py
@@ -53,7 +53,7 @@ class ScriptFangGUI(QWidget):
 
         # Payload output box (lowered)
         self.output = QTextEdit(self)
-        self.output.setGeometry(390, 360, 500, 120)
+        self.output.setGeometry(390, 430, 500, 120)
         self.output.setReadOnly(True)
         self.output.setStyleSheet(
             "background-color: rgba(0, 0, 0, 0.6); color: #00ff00; font-size: 14px; border: 2px solid #00ff00; border-radius: 10px;"
